bf16500f86b0411630b65514ee770e4a981966a3
vivo <9779930@mail.ru>
2022-05-14 00:20:48 +0300

    fix mistakes in people_gui.py

--- people_gui.py
+++ people_gui.py
@@ -2,34 +2,34 @@ from tkinter import *
 from tkinter import messagebox
 import shelve
 shelvename = 'shelve-persone'
-fildnames = ('name', 'age', 'job', 'pay')
+fieldnames = ('name', 'age', 'pay', 'job')
 
 
-def makeWidgets():
+def make_widgets():
     global enteries
     window = Tk()
     window.title('People Shelve')
     form = Frame(window)
     form.pack()
     enteries = {}
-    for (ix, label) in enumerate(('key',) + fildnames):
+    for (ix, label) in enumerate(('key',) + fieldnames):
         lab = Label(form, text=label)
         ent = Entry(form)
         lab.grid(row=ix, column=0)
         ent.grid(row=ix, column=1)
         enteries[label] = ent
-    Button(window, text='Fetch', comand=fetch_record).pack(side=LEFT)
-    Button(window, text='Update', comand=update_record).pack(side=LEFT)
-    Button(window, text='Quit', comand=window.quit).pack(side=RIGHT)
+    Button(window, text='Fetch', command=fetch_record).pack(side=LEFT)
+    Button(window, text='Update', command=update_record).pack(side=LEFT)
+    Button(window, text='Quit', command=window.quit).pack(side=RIGHT)
     return window
 
 
 def fetch_record():
     key = enteries['key'].get()
     try:
-        record = db['key']
+        record = db[key]
     except:
-        showerror(title='Error', message='No such key!')
+        messagebox.showerror(title='Error', message='No such key!')
     else:
         for field in fieldnames:
             enteries[field].delete(0, END)
@@ -38,7 +38,7 @@ def fetch_record():
 def update_record():
     key = enteries['key'].get()
     if key in db:
-        record = db['key']
+        record = db[key]
     else:
         from person import Person
         record = Person(name='?', age='?')
